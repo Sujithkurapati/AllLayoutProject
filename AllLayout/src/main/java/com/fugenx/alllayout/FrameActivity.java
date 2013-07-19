4d79ac9af7d4a7afbcc34edad9f465ed1d6a85e7
Sujithkurapati <suji4u1234@gmail.com>
2013-07-19 16:10:50 +0530

    Frame layout new
    Toast  latest commit

--- AllLayout/src/main/java/com/fugenx/alllayout/FrameActivity.java
+++ AllLayout/src/main/java/com/fugenx/alllayout/FrameActivity.java
@@ -2,6 +2,7 @@ package com.fugenx.alllayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by admin on 7/5/13.
@@ -12,6 +13,6 @@ public class FrameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame);
-        Toast.makeText(getApplicationcontext,"Frame Activity",200).show();
+        Toast.makeText(getApplicationContext(), "Frame Activity", 200).show();
     }
 }
